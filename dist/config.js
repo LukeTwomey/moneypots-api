85a5161f3be5b9c9bd756bcb1253ce614ec0c69e
Luke Twomey <luke.twomey85@gmail.com>
2018-05-16 14:34:39 +0100

    Stop tracking dist/config.js

--- dist/config.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var uri = exports.uri = 'mongodb://heroku_mwc4jn3s:a62rsg7sfai23eil3vkasfssir@ds121730.mlab.com:21730/heroku_mwc4jn3s';
