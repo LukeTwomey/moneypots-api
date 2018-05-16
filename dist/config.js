2104a19abcd92f08e9f311a9eeaa2810a252d08b
Luke Twomey <luke.twomey85@gmail.com>
2018-05-16 14:53:46 +0100

    Forgot to run build script before deploying

--- dist/config.js
+++ dist/config.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-var uri = exports.uri = ENV['MONGODB_URI'];
+var uri = exports.uri = process.env.MONGODB_URI;
