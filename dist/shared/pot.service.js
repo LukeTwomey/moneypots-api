f53d4e21784094d1eca0cecc8912b1e767542306
Luke Twomey <luke.twomey85@gmail.com>
2018-05-16 14:27:27 +0100

    Add config.js in dist folder to gitignore

--- dist/shared/pot.service.js
+++ dist/shared/pot.service.js
@@ -12,16 +12,20 @@ exports.updateSettings = updateSettings;
 exports.updateProgress = updateProgress;
 exports.updateProgressBarColor = updateProgressBarColor;
 
-// import * as config from '../config';
-var uri = 'mongodb://heroku_mwc4jn3s:a62rsg7sfai23eil3vkasfssir@ds121730.mlab.com:21730/heroku_mwc4jn3s';
+var _config = require('../config');
+
+var config = _interopRequireWildcard(_config);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+console.log(config.uri);
 
 var mongoose = require('mongoose');
 var ObjectId = require('mongodb').ObjectID;
 
 // MongoDB
 mongoose.Promise = global.Promise;
-var promise = mongoose.connect(uri);
-// var promise = mongoose.connect(config.uri);
+var promise = mongoose.connect(config.uri);
 
 promise.then(function (db) {
   console.log('MONGO CONNECTED');
